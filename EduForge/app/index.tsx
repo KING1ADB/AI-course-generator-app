20fe606a622004e4fcc5dd0d63cc7cb42d688fcf
KING1ADB <kingadb181@gmail.com>
2025-08-13 03:55:59 -0700

    Created constant folder

--- EduForge/app/index.tsx
+++ EduForge/app/index.tsx
@@ -1,12 +1,19 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
+        backgroundColor: 'white'
       }}
     >
+      <Image source={require('./../assets/images/landing.png')}
+      style={{
+        width: '100%',
+        height: 300
+      }}
+      />
 
     </View>
   );
